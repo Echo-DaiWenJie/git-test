5a10db07c88128369aa8570756f2f8f49bb87134
dai_wenjie <echo_daiwenjie@163.com>
2022-07-24 15:28:41 +0800

    third commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -2,7 +2,8 @@ package com.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!!!");
-        System.out.println("hello git2!!!");
+        System.out.println("hello git!");
+        System.out.println("hello git2!!");
+        System.out.println("hello git3!!!");
     }
 }
